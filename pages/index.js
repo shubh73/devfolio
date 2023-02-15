8280232e0a3f76735fc97ac3f3a4ce509a1e0563
Shubh Porwal <shubhporwal73@gmail.com>
2023-02-15 16:09:42 +0530

    refactor: index

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
     setClientWidth(window.innerWidth);
   }, [isDesktop, clientHeight]);
 
-  const consoleLog = () => {
+  const consoleCustomLog = () => {
     console.log(
       "%c  ____  _           _     _       ____                          _\n / ___|| |__  _   _| |__ | |__   |  _ \\ ___  _ ____      ____ _| |\n \\___ \\| '_ \\| | | | '_ \\| '_ \\  | |_) / _ \\| '__\\ \\ /\\ / / _` | |\n  ___) | | | | |_| | |_) | | | | |  __/ (_) | |   \\ V  V / (_| | |\n |____/|_| |_|\\__,_|_.__/|_| |_| |_|   \\___/|_|    \\_/\\_/ \\__,_|_|\n",
       "background: #212121; color: #6b17e8;"
@@ -93,7 +93,7 @@ export default function Home() {
             <Scripts />
           </>
         )}
-        {consoleLog()}
+        {consoleCustomLog()}
       </Meta>
     </>
   );

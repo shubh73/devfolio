4156a892d8ef8acdae6bd727639f66f370b43ace
Shubh Porwal <shubhporwal73@gmail.com>
2023-03-09 11:59:09 +0530

    feat: audiences using vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.scss';
+import { Analytics } from "@vercel/analytics/react";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
